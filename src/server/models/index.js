eb0f45e02b882155a13d8a8d801d855a66292d02
Jeisson Valder <jeissonvalderramam@gmail.com>
2017-01-06 18:49:02 -0500

    Modificación de estilos scss (footer)

---
+++ src/server/models/index.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const co = require('co')
+const r = require('rethinkdb')
+const Promise = require('bluebird')
+const utils = require('./utils')
+const uuid = require('uuid-base62')
+
+const defaults = {
+  host: 'localhost',
+  port: 28015,
+  db: 'platzigram'
+}
+
+
+class Db { // Begin Db class
+  constructor (options) {
+    options = options || {}
+    this.host = options.host || defaults.host
+    this.port = options.port || defaults.port
+    this.db = options.db || defaults.db
+  }
+
+  connect (callback) {
+    this.connection = r.connect({
+      host: this.host,
+      port: this.port
+    })
+
+    this.connected = true
+
+    let db = this.db
+    let connection = this.connection
+
+    let setup = co.wrap(function * () {
+      let conn = yield connection
+
+      let dbList = yield r.dbList().run(conn)
+      if (dbList.indexOf(db) === -1) {
+        yield r.dbCreate(db).run(conn)
+      }
+
+      let dbTables = yield r.db(db).tableList().run(conn)
+      if (dbTables.indexOf('images') === -1) {
+        yield r.db(db).tableCreate('images').run(conn)
+        yield r.db(db).table('images').indexCreate('createdAt').run(conn)
+      }
+
+      if (dbTables.indexOf('users') === -1) {
+        yield r.db(db).tableCreate('users').run(conn)
+        yield r.db(db).table('users').indexCreate('username').run(conn)
+      }
+
+      return conn
+    })
+
+    return Promise.resolve(setup()).asCallback(callback)
+  }
+
+  saveComment (text, callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('not connected')).asCallback(callback)
+    }
+
+    let connection = this.connection
+    let db = this.db
+
+    let tasks = co.wrap(function * () {
+      let conn = yield connection
+      user.password = utils.encrypt(user.password)
+      user.createdAt = new Date()
+
+      let result = yield r.db(db).table('users').insert(user).run(conn)
+
+      if (result.errors > 0) {
+        return Promise.reject(new Error(result.first_error))
+      }
+
+      user.id = result.generated_keys[0]
+
+      let created = yield r.db(db).table('users').get(user.id).run(conn)
+
+      return Promise.resolve(created)
+    })
+
+    return Promise.resolve(tasks()).asCallback(callback)
+  }
+} // End Db class
+
+module.exports = Db
